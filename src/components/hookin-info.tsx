dfc1be1fbc03a3f95db2f8da4a78e5e9cb07b8ba
riperk <riperk@protonmail.com>
2019-05-12 01:14:26 +0000

    improve hookin info

--- src/components/hookin-info.tsx
+++ src/components/hookin-info.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { Link } from 'react-router-dom';
 import { wallet, useHookin } from '../state/wallet';
 
 export default function HookinInfo(props: RouteComponentProps<{ hash: string }>) {
@@ -13,9 +14,39 @@ export default function HookinInfo(props: RouteComponentProps<{ hash: string }>)
 
   return (
     <div>
-      <pre>
-        <code>{JSON.stringify(hookinDoc, null, 2)}</code>
-      </pre>
+      <table className="table">
+        <tbody>
+          <tr>
+            <th>hash</th>
+            <td>
+              <Link to={`/hookins/${hookinDoc.hash}`}>{hookinDoc.hash}</Link>
+            </td>
+          </tr>
+          <tr>
+            <th>bitcoin address</th>
+            <td>
+              <Link to={`/addresses/bitcoin/${hookinDoc.bitcoinAddress}`}>{hookinDoc.bitcoinAddress}</Link>
+            </td>
+          </tr>
+          <tr>
+            <th>amount</th>
+            <td>{hookinDoc.amount} sat</td>
+          </tr>
+          <tr>
+            <th>tx</th>
+            <td>
+              <a href={`https://blockstream.info/testnet/tx/${hookinDoc.txid}?output:${hookinDoc.vout}`} target="_blank" >{hookinDoc.txid}:{hookinDoc.vout}</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <hr/>
+      <h3>Raw Hookin</h3>
+      <div>
+        <pre>
+          <code>{JSON.stringify(hookinDoc, null, 2)}</code>
+        </pre>
+      </div>
     </div>
   );
 }
