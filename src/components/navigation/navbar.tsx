b4590a3cd2d761c44b48d1ce9c306f9345d90ce9
sugarfreecow <chickenlittle@gmail.com>
2019-05-14 17:44:07 -0700

    Modified custom-navbar icons

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,21 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './navbar.scss';
 import { Link, withRouter } from 'react-router-dom';
 
 export default withRouter(NavBar);
 
 function NavBar(props: any) {
-  const [isDashboardHover, setIsDashboardHover] = useState(false);
-  const dashboardHover = { onMouseEnter: () => setIsDashboardHover(true), onMouseLeave: () => setIsDashboardHover(false) };
 
-  const [isReceiveHover, setIsReceiveHover] = useState(false);
-  const receiveHover = { onMouseEnter: () => setIsReceiveHover(true), onMouseLeave: () => setIsReceiveHover(false) };
-
-  const [isSendHover, setIsSendHover] = useState(false);
-  const sendHover = { onMouseEnter: () => setIsSendHover(true), onMouseLeave: () => setIsSendHover(false) };
-
-  const [isHistoryHover, setIsHistoryHover] = useState(false);
-  const historyHover = { onMouseEnter: () => setIsHistoryHover(true), onMouseLeave: () => setIsHistoryHover(false) };
 
   function NavLink(props: any) {
     let isActive = props.path === props.to;
@@ -23,10 +13,7 @@ function NavBar(props: any) {
 
     return <Link id={activeId} {...props} />;
   }
-  let hoverDashboardClassName = isDashboardHover ? 'dashboard-hover' : '';
-  let hoverReceiveClassName = isReceiveHover ? 'receive-hover' : '';
-  let hoverSendClassName = isSendHover ? 'send-hover' : '';
-  let hoverHistoryClassName = isHistoryHover ? 'history-hover' : '';
+
 
   return (
     <div className="custom-navbar">
@@ -40,20 +27,20 @@ function NavBar(props: any) {
           <p>v 0.1</p>
         </div>
       )}
-      <NavLink to="/" path={props.location.pathname} className={hoverDashboardClassName} {...dashboardHover}>
-        <div className="navbar-img-container dashboard" />
+      <NavLink to="/" path={props.location.pathname}>
+        <i className="fal fa-desktop-alt"/>
         <span className="navbar-link-text">Dashboard</span>
       </NavLink>
-      <NavLink path={props.location.pathname} to="/receive" className={hoverReceiveClassName} {...receiveHover}>
-        <div className="navbar-img-container receive" />
+      <NavLink path={props.location.pathname} to="/receive">
+        <i className="fal fa-inbox-in"/>
         <span className="navbar-link-text">Receive</span>
       </NavLink>
-      <NavLink path={props.location.pathname} to="/send" className={hoverSendClassName} {...sendHover}>
-        <div className="navbar-img-container send" />
+      <NavLink path={props.location.pathname} to="/send">
+        <i className="fal fa-inbox-out"/>
         <span className="navbar-link-text">Send</span>
       </NavLink>
-      <NavLink path={props.location.pathname} to="/history" className={hoverHistoryClassName} {...historyHover}>
-        <div className="navbar-img-container history" />
+      <NavLink path={props.location.pathname} to="/history">
+        <i className="fal fa-history"/>
         <span className="navbar-link-text">History</span>
       </NavLink>
     </div>
